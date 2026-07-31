770212f57ca1bbcfc74bced7ae104eb2d65e0b47
Angélica Anjos <angellica.anjos@hotmail.com>
2026-07-31 15:31:59 -0300

    feat: adicionar responsividade à sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,14 +6,22 @@ import { LuTags } from "react-icons/lu";
 import { FiLogOut } from "react-icons/fi";
 import { FaRegUserCircle } from "react-icons/fa";
 
-function Sidebar() {
+function Sidebar({ isOpen }) {
   return (
-    <aside className="bg-primary w-60 min-h-screen py-3 flex flex-col">
-      <div className="flex justify-center gap-2 items-center p-4">
+    <aside
+      className={`bg-primary min-h-screen py-3 flex flex-col transition-all duration-300 ${
+        isOpen ? "w-60" : "w-20"
+      }`}
+    >
+      <div
+        className={`flex items-center p-4 ${
+          isOpen ? "justify-center gap-2" : "justify-center"
+        }`}
+      >
         <img className="w-10" src={Logo} alt="logo" />
-        <h2 className="text-surface text-center text-2xl font-semibold">
-          TechStock
-        </h2>
+        {isOpen && (
+          <h2 className="text-surface text-2xl font-semibold">TechStock</h2>
+        )}
       </div>
 
       <hr className="text-surface mx-4" />
@@ -24,13 +32,15 @@ function Sidebar() {
             <NavLink
               to="/dashboard"
               className={({ isActive }) =>
-                `flex items-center gap-2 text-surface text-lg py-2 px-4 rounded-lg ${
+                `flex items-center ${
+                  isOpen ? "gap-2 px-4" : "justify-center px-0"
+                } text-surface text-lg py-2 rounded-lg ${
                   isActive ? "bg-[#4C69BB]" : "hover:bg-[#4C69BB]"
                 }`
               }
             >
               <FaRegChartBar size={20} />
-              Dashboard
+              {isOpen && <span>Dashboard</span>}
             </NavLink>
           </li>
 
@@ -38,13 +48,15 @@ function Sidebar() {
             <NavLink
               to="/produtos"
               className={({ isActive }) =>
-                `flex items-center gap-2 text-surface text-lg py-2 px-4 rounded-lg ${
+                `flex items-center ${
+                  isOpen ? "gap-2 px-4" : "justify-center px-0"
+                } text-surface text-lg py-2 rounded-lg ${
                   isActive ? "bg-[#4C69BB]" : "hover:bg-[#4C69BB]"
                 }`
               }
             >
               <BiPackage size={20} />
-              Produtos
+              {isOpen && <span>Produtos</span>}
             </NavLink>
           </li>
 
@@ -52,29 +64,40 @@ function Sidebar() {
             <NavLink
               to="/categorias"
               className={({ isActive }) =>
-                `flex items-center gap-2 text-surface text-lg py-2 px-4 rounded-lg ${
+                `flex items-center ${
+                  isOpen ? "gap-2 px-4" : "justify-center px-0"
+                } text-surface text-lg py-2 rounded-lg ${
                   isActive ? "bg-[#4C69BB]" : "hover:bg-[#4C69BB]"
                 }`
               }
             >
               <LuTags size={20} />
-              Categorias
+              {isOpen && <span>Categorias</span>}
             </NavLink>
           </li>
         </ul>
       </nav>
 
       <div className="mt-auto p-4">
-        <div className="flex items-center gap-2 px-4 mb-3">
+        <div
+          className={`flex items-center mb-3 ${
+            isOpen ? "gap-2 px-4" : "justify-center"
+          }`}
+        >
           <FaRegUserCircle className="text-surface" size={22} />
-          <p className="text-surface font-medium">Administrador</p>
+          {isOpen && <p className="text-surface font-medium">Administrador</p>}
         </div>
 
         <hr className="my-3 border-surface/20" />
 
-        <div className="flex items-center gap-2 text-surface text-lg py-2 px-4 transition-transform duration-300 hover:scale-105 cursor-pointer">
+        <div
+          className={`flex items-center text-surface text-lg py-2 rounded-lg cursor-pointer transition-transform duration-300 hover:scale-105 ${
+            isOpen ? "gap-2 px-4" : "justify-center"
+          }`}
+        >
           <FiLogOut size={20} />
-          <NavLink to="/">Sair</NavLink>
+
+          {isOpen && <NavLink to="/">Sair</NavLink>}
         </div>
       </div>
     </aside>
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,26 @@
 import Sidebar from "../components/Sidebar";
 import { Outlet } from "react-router-dom";
+import { useState } from "react";
+import { MdKeyboardArrowLeft } from "react-icons/md";
+import { MdKeyboardArrowRight } from "react-icons/md";
 
 function MainLayout() {
+  const [isOpen, setIsOpen] = useState(true);
+
   return (
     <div className="flex min-h-screen">
-      <Sidebar />
+      <Sidebar isOpen={isOpen} />
+
+      <button
+        className="bg-primary-light text-surface rounded-r-full h-10 mt-4 shadow-md cursor-pointer"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {isOpen ? (
+          <MdKeyboardArrowLeft size={22} />
+        ) : (
+          <MdKeyboardArrowRight size={22} />
+        )}
+      </button>
 
       <main className="flex">
         <Outlet />
