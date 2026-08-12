5767b0b2c226b4b5247c431638a6b844e047a831
Angélica Anjos <angellica.anjos@hotmail.com>
2026-08-12 18:02:30 -0300

    feat: cria página de categorias

---
+++ src/components/table/CategoryColumns.jsx
@@ -0,0 +1,39 @@
+import { FiEdit3, FiTrash2 } from "react-icons/fi";
+
+export const columns = [
+  {
+    accessorKey: "name",
+    header: "Categorias",
+  },
+  {
+    accessorKey: "description",
+    header: "Descrição",
+  },
+  {
+    accessorKey: "products",
+    header: "Produtos",
+  },
+  {
+    id: "actions",
+    header: "Ações",
+    cell: ({ row }) => (
+      <div className="flex items-center gap-2">
+        <button
+          className="border rounded-lg p-2 text-primary-light transition-colors hover:bg-blue-50 cursor-pointer"
+          title="Editar"
+          onClick={() => console.log("Editar", row.original)}
+        >
+          <FiEdit3 size={18} />
+        </button>
+
+        <button
+          className="border rounded-lg p-2 text-red-600 transition-colors hover:bg-red-50 cursor-pointer"
+          title="Excluir"
+          onClick={() => console.log("Excluir", row.original)}
+        >
+          <FiTrash2 size={18} />
+        </button>
+      </div>
+    ),
+  },
+];
--- src/pages/Categorias.jsx
+++ src/pages/Categorias.jsx
@@ -1,7 +1,72 @@
+import Input from "../components/ui/Input";
+import Button from "../components/ui/Button";
+import { LuPlus } from "react-icons/lu";
+import DataTable from "../components/table/DataTable";
+import { columns } from "../components/table/CategoryColumns";
+import Modal from "../components/ui/Modal";
+import { useState } from "react";
+
+const data = [
+  {
+    name: "Computadores",
+    description: "Computadores e notebooks",
+    products: 32,
+  },
+  {
+    name: "Monitores",
+    description: "Monitores e telas",
+    products: 18,
+  },
+  {
+    name: "Acessórios",
+    description: "Periféricos e acessórios",
+    products: 45,
+  },
+];
+
 function Categorias() {
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
   return (
-    <div>
-      <h1>Categorias</h1>
+    <div className="ml-3 mr-6 my-6">
+      <h1 className="text-text text-3xl font-semibold">Categorias</h1>
+      <p>Gerencie as categorias dos seus produtos.</p>
+
+      <div className="mt-6 flex flex-col gap-3 lg:flex-row lg:items-center">
+        <div className="grid flex-1 gap-3 md:grid-cols-3 lg:max-w-225">
+          <Input type="search" placeholder="Buscar categoria..." />
+        </div>
+
+        <Button
+          className="flex items-center justify-center gap-2 lg:ml-auto"
+          onClick={() => setIsOpenModal(!isOpenModal)}
+        >
+          <LuPlus size={18} />
+          Nova categoria
+        </Button>
+      </div>
+
+      <div className="mt-6 overflow-x-auto">
+        <DataTable columns={columns} data={data} />
+      </div>
+
+      <Modal
+        isOpenModal={isOpenModal}
+        onClose={() => setIsOpenModal(false)}
+        title="Nova categoria"
+        description="Adicione uma nova categoria ao estoque."
+        submitText="Adicionar"
+      >
+        <div className="col-span-2">
+          <label>Nome da categoria</label>
+          <Input type="text" placeholder="Nome da categoria" />
+        </div>
+
+        <div className="col-span-2">
+          <label>Descrição</label>
+          <Input type="text" placeholder="Descrição" />
+        </div>
+      </Modal>
     </div>
   );
 }
